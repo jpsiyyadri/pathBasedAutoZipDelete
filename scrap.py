06ba36b38e4719d7e00bb2d9a4396ffd49e376cd
Jai Prakash <jpsiyyadri@gmail.com>
2021-01-28 18:01:44 +0530

    UPD: *.txt logic

--- scrap.py
+++ scrap.py
@@ -3,6 +3,7 @@ import os
 import pathlib
 from datetime import datetime, timedelta
 import logging
+import glob
 
 logging.basicConfig(filename='{}_logs.log'.format(datetime.now().strftime("%d_%m_%Y")), format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',datefmt='%H:%M:%S',level=logging.DEBUG,filemode='a')
 AUDIT_FILE_CONTENTS = []
@@ -14,10 +15,11 @@ def delete_files(file_names=[]):
         else:
             print("*** The file does not exist ***")
 
-def create_zip_file(file_names=[]):
+def create_zip_file(file_names=[], zip_file_name='sample'):
+    print(file_names)
     if(len(file_names)):
         # create a ZipFile object
-        zipObj = ZipFile('sample.zip', 'w')
+        zipObj = ZipFile('{}.zip'.format(zip_file_name), 'w')
         # Add multiple files to the zip
         for fyle in file_names:
             zipObj.write(fyle)
@@ -34,20 +36,22 @@ def prepare_zip_files():
             for t in txt:
                 extracted_path = t.strip().split('|')
                 if(len(extracted_path)>0):
-                    fname = pathlib.Path(extracted_path[1])
+                    path = extracted_path[1]
                     retention_period = int(extracted_path[2])
-                    AUDIT_FILE_CONTENTS.append({
-                        "file_name": extracted_path[1],
-                        "retention_period": retention_period
-                    })
-                    assert fname.exists(), f'No such file: {fname}'
-                    # check if the file older than the days mentioned
-                    logging.info("# check if the file older than the days mentioned")                    
-                    file_created_time = datetime.fromtimestamp(fname.stat().st_mtime)
-                    if(datetime.today() - timedelta(days=retention_period) >=  file_created_time):
-                        files_to_zip.append(extracted_path[1])
+                    for filename in glob.glob(path):
+                        fname = pathlib.Path(filename)
+                        AUDIT_FILE_CONTENTS.append({
+                            "file_name": filename,
+                            "retention_period": retention_period
+                        })
+                        assert fname.exists(), f'No such file: {fname}'
+                        # check if the file older than the days mentioned
+                        logging.info("# check if the file older than the days mentioned")
+                        file_created_time = datetime.fromtimestamp(fname.stat().st_mtime)
+                        if(datetime.today() - timedelta(days=retention_period) >= file_created_time):
+                            files_to_zip.append(filename)
     
-            create_zip_file(files_to_zip)
+            create_zip_file(files_to_zip, 'zipped_sample')
     finally:
         f.close()
 
@@ -59,18 +63,20 @@ def prepare_delete_files():
             for t in txt:
                 extracted_path = t.strip().split('|')
                 if(len(extracted_path)>0):
-                    fname = pathlib.Path(extracted_path[1])
+                    path = extracted_path[1]
                     retention_period = int(extracted_path[2])
-                    AUDIT_FILE_CONTENTS.append({
-                        "file_name": extracted_path[1],
-                        "retention_period": retention_period
-                    })
-                    assert fname.exists(), f'No such file: {fname}'
-                    # check if the file older than the days mentioned
-                    logging.info("# check if the file older than the days mentioned")
-                    file_created_time = datetime.fromtimestamp(fname.stat().st_mtime)
-                    if(datetime.today() - timedelta(days=retention_period) >= file_created_time):
-                        files_to_delete.append(extracted_path[1])
+                    for filename in glob.glob(path):
+                        fname = pathlib.Path(filename)
+                        AUDIT_FILE_CONTENTS.append({
+                            "file_name": filename,
+                            "retention_period": retention_period
+                        })
+                        assert fname.exists(), f'No such file: {fname}'
+                        # check if the file older than the days mentioned
+                        logging.info("# check if the file older than the days mentioned")
+                        file_created_time = datetime.fromtimestamp(fname.stat().st_mtime)
+                        if(datetime.today() - timedelta(days=retention_period) >= file_created_time):
+                            files_to_delete.append(filename)
             delete_files(files_to_delete)
             logging.info("File successfully purged")
     finally:
@@ -85,6 +91,11 @@ def write_into_audit_file():
     finally:
         f.close()
 
+def test():
+    path = "data/file_1.txt"
+    for filename in glob.glob(path):
+        with open(os.path.join(os.getcwd(), filename), 'r') as f: # open in readonly mode
+            print(filename)
 
 if __name__=="__main__":
     # zip files
@@ -93,3 +104,4 @@ if __name__=="__main__":
     prepare_delete_files()
     # audit file
     write_into_audit_file()
+    # test()
